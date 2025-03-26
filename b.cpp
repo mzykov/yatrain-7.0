8edb02a5e65a002ad6f9ac589ca3d1a2f58df157
Mikhail Zykov <zmsmihail@yandex.ru>
2025-03-26 14:35:13 +0300

    task B - first test passed

--- b.cpp
+++ b.cpp
@@ -1,14 +1,15 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
 int reduced_time(std::vector<std::vector<int>>& times) {
     constexpr int max_duration = 3600;
     std::vector<int> dp(3, max_duration);
-    q[0] = 0;
+    dp[0] = 0;
 
     while (!times.empty()) {
         const auto& curr = times.back();
-        int t_next = min({ curr[0] + dp[0], curr[1] + dp[1], curr[2] + dp[2] });
+        int t_next = std::min({ curr[0] + dp[0], curr[1] + dp[1], curr[2] + dp[2] });
         dp.insert(dp.begin(), t_next);
         dp.pop_back();
         times.pop_back();
@@ -21,7 +22,7 @@ int main() {
     int n = 1;
     std::cin >> n;
 
-    vector<vector<int>> times;
+    std::vector<std::vector<int>> times;
 
     while (n--) {
         int a, b, c;
